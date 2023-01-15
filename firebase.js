2a77672293386ea2e3289abe36c5f609b6727a08
Hajiman <hjm2639@naver.com>
2023-01-15 10:08:41 +0900

    1.1

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBhQKBUOiYtwnu3tV1W33vW6ZnATAFtR9Q",
+  authDomain: "fir-practice-2a5b1.firebaseapp.com",
+  projectId: "fir-practice-2a5b1",
+  storageBucket: "fir-practice-2a5b1.appspot.com",
+  messagingSenderId: "769842447250",
+  appId: "1:769842447250:web:c01717532fc063c1f2774b",
+};
+
+export default firebase.initializeApp(firebaseConfig);
